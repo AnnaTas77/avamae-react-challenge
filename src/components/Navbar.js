fd726e9ce0a1d0e485df05ff70fd87bdc8b15cd1
Anna Tasheva <anna.tasheva90@gmail.com>
2023-06-18 10:42:40 +0100

    Added a contact us form with css grid styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,31 +5,33 @@ import Logo from "../resources/Logo.svg";
 
 const Navbar = () => {
     return (
-        <nav className="nav-container">
-            <div className="nav-wrapper">
-                <Link to="/">
-                    <img src={Logo} className="logo-image" alt="Logo" />
-                </Link>
-                <ul className="link-container">
-                    <li className="nav-link">
-                        <NavLink to="/" className="nav-link">
-                            Home
-                        </NavLink>
-                    </li>
-                    <li className="nav-link">
-                        <NavLink to="/about-us" className="nav-link">
-                            About Us
-                        </NavLink>
-                    </li>
-                    <li className="nav-link">
-                        <NavLink to="/contact-us" className="nav-link">
-                            Contact Us
-                        </NavLink>
-                    </li>
-                    <button className="login-btn">Log In</button>
-                </ul>
-            </div>
-        </nav>
+        <div className="nav-root">
+            <nav className="nav-container">
+                <div className="nav-wrapper">
+                    <Link to="/">
+                        <img src={Logo} className="logo-image" alt="Logo" />
+                    </Link>
+                    <ul className="link-container">
+                        <li className="nav-link">
+                            <NavLink to="/" className="nav-link">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink to="/about-us" className="nav-link">
+                                About Us
+                            </NavLink>
+                        </li>
+                        <li className="nav-link">
+                            <NavLink to="/contact-us" className="nav-link">
+                                Contact Us
+                            </NavLink>
+                        </li>
+                        <button className="login-btn">Log In</button>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     );
 };
 
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,72 +1,157 @@
-import React from "react";
+import { React, useState } from "react";
 import "./contact.css";
 
 const ContactUs = () => {
+    const [addressFieldsVisibility, setAddressFieldsVisibility] =
+        useState(false);
+
+    function toggleAddressFields() {
+        setAddressFieldsVisibility(!addressFieldsVisibility);
+    }
+
     return (
-        <div>
-            <h2 className="contact-us">Contact Us</h2>
-            <p className="lorem-paragraph">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit quisque
-                faucibus ex sapien vitae. Adipiscing elit quisque faucibus ex
-                sapien vitae pellentesque sem placerat in id cursus.
-            </p>
-
-            <form className="contact-form">
-                <div className="form-first-row">
-                    <div className="column-name">
+        <div className="contact-container">
+            <div className="contact-wrapper">
+                <h2 className="contact-us">Contact Us</h2>
+                <p className="lorem-paragraph">
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit
+                    quisque faucibus ex sapien vitae. Adipiscing elit quisque
+                    faucibus ex sapien vitae pellentesque sem placerat in id
+                    cursus.
+                </p>
+                <form className="contact-form">
+                    <div className="name">
                         <div>
-                            <label htmlFor="name">Full name:</label>
+                            <label htmlFor="name">Full name</label>
                         </div>
-                        <div>
-                            <input type="text" id="name" name="name" required />
-                        </div>
+                        <input type="text" id="name" name="name" required />
                     </div>
-                    <div className="column-email">
+                    <div className="email">
                         <div>
-                            <label htmlFor="email">Email address:</label>
+                            <label htmlFor="email">Email address</label>
                         </div>
-                        <div>
-                            <input
-                                type="text"
-                                id="email"
-                                name="email"
-                                required
-                            />
-                        </div>
+                        <input type="text" id="email" name="email" required />
                     </div>
-                </div>
+                    <div className="phone">
+                        <div>
+                            <label htmlFor="phone">
+                                Phone number 01
+                                <span className="optional"> - optional</span>
+                            </label>
+                        </div>
+                        <input type="text" id="phone" name="phone" />
+                    </div>
 
-                <label htmlFor="phone">
-                    <p>
-                        Phone number 01{" "}
-                        <span className="optional"> - optional</span>
-                    </p>
-                </label>
-                <input type="text" id="phone" name="phone" />
+                    <button className="add-number-btn">
+                        Add new phone number
+                    </button>
+                    <div className="message">
+                        <div>
+                            <label htmlFor="message">Message</label>
+                            <span>Maximum text length is 500 characters</span>
+                        </div>
+                        <textarea
+                            id="message"
+                            name="message"
+                            rows="10"
+                            required
+                        ></textarea>
+                    </div>
 
-                <button className="add-number-btn">Add new phone number</button>
-
-                <label htmlFor="message">Message:</label>
-                <textarea
-                    id="message"
-                    name="message"
-                    rows="10"
-                    required
-                ></textarea>
-                <div>
                     <div className="checkbox-btn">
                         <input
                             type="checkbox"
                             id="address"
                             name="address_details"
-                            value="address"
+                            value="address_details"
+                            onChange={toggleAddressFields}
                         ></input>
                         <label htmlFor="address">Add address details</label>
-                        <div class="control"></div>
                     </div>
-                </div>
-                <button className="submit-btn">Submit</button>
-            </form>
+                    {addressFieldsVisibility ? (
+                        <div className="address-container">
+                            <div className="address-1">
+                                <div>
+                                    <label htmlFor="address-1">
+                                        Address line 1
+                                    </label>
+                                </div>
+                                <input
+                                    type="text"
+                                    id="address-1"
+                                    name="address-1"
+                                    required
+                                />
+                            </div>
+                            <div className="address-2">
+                                <div>
+                                    <label htmlFor="address-2">
+                                        Address line 2
+                                        <span className="optional">
+                                            {" "}
+                                            - optional
+                                        </span>
+                                    </label>
+                                </div>
+                                <input
+                                    type="text"
+                                    id="address-2"
+                                    name="address-2"
+                                />
+                            </div>
+                            <div className="city-town">
+                                <div>
+                                    <label htmlFor="city-town">City/Town</label>
+                                </div>
+                                <input
+                                    type="text"
+                                    id="city-town"
+                                    name="city-town"
+                                />
+                            </div>
+                            <div className="state-county">
+                                <div>
+                                    <label htmlFor="state-county">
+                                        State/County
+                                    </label>
+                                </div>
+
+                                <input
+                                    type="text"
+                                    id="state-county"
+                                    name="state-county"
+                                />
+                            </div>
+                            <div className="postcode">
+                                <div>
+                                    <label htmlFor="postcode">Postcode</label>
+                                </div>
+                                <input
+                                    type="text"
+                                    id="postcode"
+                                    name="postcode"
+                                />
+                            </div>
+
+                            <div className="country">
+                                <div>
+                                    <label htmlFor="country">Country</label>
+                                </div>
+                                <input
+                                    type="text"
+                                    id="country"
+                                    name="country"
+                                />
+                            </div>
+                        </div>
+                    ) : (
+                        ""
+                    )}
+
+                    <button className="submit-btn">Submit</button>
+                </form>
+            </div>
+            <div className="logo-container"></div>
         </div>
     );
 };
